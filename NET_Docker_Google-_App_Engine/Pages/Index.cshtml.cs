39aeb693a7a76e86bb4c24de5ba3e2282a59de2b
Matt Grofsky <matt@ytel.com>
2019-10-17 13:45:42 -0700

    Initial Commit

--- NET_Docker_Google-_App_Engine/Pages/Index.cshtml.cs
+++ NET_Docker_Google-_App_Engine/Pages/Index.cshtml.cs
@@ -9,9 +9,15 @@ namespace NET_Docker_Google__App_Engine.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; private set; } = "Hi there!";
+        public string Companies { get; private set; } = "";
+        public string Name { get; private set; } = "No One!";
+
         public void OnGet()
         {
-
+            
+            if (!String.IsNullOrEmpty(HttpContext.Request.Query["name"]))
+                Name = HttpContext.Request.Query["name"];
         }
     }
 }
