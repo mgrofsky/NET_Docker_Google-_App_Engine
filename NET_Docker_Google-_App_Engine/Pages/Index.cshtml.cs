373c56f7e5e328329162658eb2df3721bc0f5f32
Matt Grofsky <matt@ytel.com>
2019-10-17 13:44:33 -0700

    Initial Commit

---
+++ NET_Docker_Google-_App_Engine/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NET_Docker_Google__App_Engine.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
